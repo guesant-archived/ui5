25eea1bac9219ababdf0c7bf02d897bba1bfebc2
Gabriel Rodrigues <jipacoding@gmail.com>
2021-03-11 15:58:53 -0400

    feat: Editor: IEditorContextState

--- ui5/src/Components/Editor/Hooks/IEditorContextState.tsx
+++ ui5/src/Components/Editor/Hooks/IEditorContextState.tsx
@@ -1,2 +1,11 @@
+import { IEditorProject, ISetter } from "@guesant/ui5-lib";
+
 export type IEditorContextState = {
+  projects: IEditorProject[];
+  currentProject: IEditorProject | null;
+  currentProjectIndex: number;
+  updateProject: (projectIndex: number, data: IEditorProject) => boolean;
+  updateCurrentProject: (data: IEditorProject) => boolean;
+  setProjects: ISetter<IEditorContextState["projects"]>;
+  setCurrentProjectIndex: ISetter<number>;
 };
