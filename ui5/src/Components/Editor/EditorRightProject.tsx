c74e295f36227ffec1f960a839ad508c2e7dd9f5
Gabriel Rodrigues <jipacoding@gmail.com>
2021-03-13 14:20:36 -0400

    feat: Editor: EditorRightProjectID

--- ui5/src/Components/Editor/EditorRightProject.tsx
+++ ui5/src/Components/Editor/EditorRightProject.tsx
@@ -1,15 +1,36 @@
+import { TextInput } from "carbon-components-react";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 import { useContext } from "react";
 import styles from "./Editor.module.css";
 import { EditorContext } from "./Hooks/EditorContext";
 
+const EditorRightProjectID = () => {
+  const [[idFieldProjectID]] = useState(() => [nanoid()] as const);
+  const { currentProject } = useContext(EditorContext);
+  return (
+    <>
+      <div>
+        <TextInput
+          readOnly
+          labelText="ID"
+          id={"id" + idFieldProjectID}
+          value={currentProject!.meta?.id}
+          onFocus={(e) => e.target.select()}
+        />
+      </div>
+    </>
+  );
+};
+
 const EditorRightProject = () => {
   const { currentProject } = useContext(EditorContext);
-
   return (
     <>
       {currentProject && (
         <>
           <div className={styles.editProject}>
+            <EditorRightProjectID />
           </div>
         </>
       )}
