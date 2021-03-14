9b4a0ba4d0e46bf908913b0efe831fefae386b4f
Gabriel Rodrigues <jipacoding@gmail.com>
2021-03-14 08:34:05 -0400

    feat(lib): ProjectAssets: isValidImage

--- ui5-lib/src/ProjectAssets.ts
+++ ui5-lib/src/ProjectAssets.ts
@@ -6,4 +6,11 @@ export class ProjectAssets {
     width: number;
     height: number;
   }) => `https://picsum.photos/${width}/${height}`;
+  static isValidImage = (url: string) =>
+    new Promise<boolean>((resolve) => {
+      const image = new Image();
+      image.addEventListener("load", () => resolve(true));
+      image.addEventListener("error", () => resolve(false));
+      image.src = url;
+    }).catch(() => false);
 }
